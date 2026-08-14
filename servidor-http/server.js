79e9131b5629418960f2564ad274a0a3c0289817
Helder Gabriel <125323362+helred1221@users.noreply.github.com>
2026-08-14 19:03:13 +0000

    edit: alterei código do arquivo do servidor.

--- servidor-http/server.js
+++ servidor-http/server.js
@@ -1,17 +1,19 @@
-const http = require('node:http')
+import http from 'node:http'
 
 const porta = 3000
 
 const server = http.createServer();
 
-server.on('request', (req, res) => {
+const requisicao = (req, res) => {
     console.log(`Requisição recebida! ${req.method} ${req.url}`);
 
     res.statusCode = 200
-    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-    res.end("Servidor Funcionando!");    
-});
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({"chave":"valor"}));
+}
 
-server.listen(porta, ()=> {
+server.on('request', requisicao);
+
+server.listen(porta, () => {
     console.log(`Servidor ouvindo na porta ${porta}`)
 });
